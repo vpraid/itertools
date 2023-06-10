63ccaee3e6977e8510d8437db4fb75b624577a83
Valerii Praid <valerii.praid@gmail.com>
2023-06-10 10:06:21 +0200

    Implement map iterator

---
+++ iterator/map.go
@@ -0,0 +1,25 @@
+package iterator
+
+// MapIterator is an iterator that applies a function to each element of the underlying iterator.
+type MapIterator[T any, U any] struct {
+	it Iterator[T]
+	fn func(T) U
+}
+
+// Map returns a MapIterator that applies fn to each element of the underlying iterator.
+func Map[T any, U any](it Iterator[T], fn func(t T) U) *MapIterator[T, U] {
+	return &MapIterator[T, U]{
+		it: it,
+		fn: fn,
+	}
+}
+
+// Next advances the iterator to the next element. It returns false if the underlying iterator was exhausted.
+func (mi *MapIterator[T, U]) Next() bool {
+	return mi.it.Next()
+}
+
+// Value returns the current element of the underlying iterator after applying the mapping function.
+func (mi *MapIterator[T, U]) Value() U {
+	return mi.fn(mi.it.Value())
+}
---
+++ iterator/map_test.go
@@ -0,0 +1,46 @@
+package iterator
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMap_Empty(t *testing.T) {
+	t.Parallel()
+
+	s := Slice([]string{})
+	m := Map[string, int](s, func(t string) int { return len(t) })
+	assert.False(t, m.Next())
+	assert.Equal(t, 0, m.Value())
+}
+
+func TestMap_NonEmpty(t *testing.T) {
+	t.Parallel()
+
+	s := Slice([]string{"a", "bb", "ccc"})
+	m := Map[string, int](s, func(t string) int { return len(t) })
+	assert.True(t, m.Next())
+	assert.Equal(t, 1, m.Value())
+	assert.True(t, m.Next())
+	assert.Equal(t, 2, m.Value())
+	assert.True(t, m.Next())
+	assert.Equal(t, 3, m.Value())
+	assert.False(t, m.Next())
+	assert.Equal(t, 0, m.Value())
+}
+
+func ExampleMap() {
+	s := Slice([]string{"a", "bb", "ccc"})
+	// We need to specify the type of the iterator explicitly because the compiler cannot infer it yet. This is a known
+	// limitation of the Go compiler which will be fixed in Go 1.21.
+	m := Map[string](s, func(t string) int { return len(t) })
+	for m.Next() {
+		fmt.Println(m.Value())
+	}
+	// Output:
+	// 1
+	// 2
+	// 3
+}
