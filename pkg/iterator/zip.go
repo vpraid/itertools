2f45fb1a8de4f2c265a222caf0ad513bebe44a6b
Valerii Praid <valerii.praid@gmail.com>
2023-06-10 22:04:19 +0200

    Implement zip iterator

---
+++ pkg/iterator/zip.go
@@ -0,0 +1,36 @@
+package iterator
+
+// ZipIterator is an iterator that iterates over two iterators in parallel.
+type ZipIterator[T, U any] struct {
+	it1 Iterator[T]
+	it2 Iterator[U]
+}
+
+// Zip returns a ZipIterator.
+func Zip[T, U any](it1 Iterator[T], it2 Iterator[U]) *ZipIterator[T, U] {
+	return &ZipIterator[T, U]{
+		it1: it1,
+		it2: it2,
+	}
+}
+
+// Next advances the iterator to the next element. It returns false if one of the underlying iterators was exhausted.
+func (zi *ZipIterator[T, U]) Next() bool {
+	return zi.it1.Next() && zi.it2.Next()
+}
+
+// Pair represents a pair of elements from two iterators.
+type Pair[T, U any] struct {
+	First  T
+	Second U
+}
+
+// Value returns the current element.
+func (zi *ZipIterator[T, U]) Value() Pair[T, U] {
+	return Pair[T, U]{First: zi.it1.Value(), Second: zi.it2.Value()}
+}
+
+// Collect returns the elements of the underlying iterator as a slice.
+func (zi *ZipIterator[T, U]) Collect() []Pair[T, U] {
+	return CollectFromIter[Pair[T, U]](zi)
+}
---
+++ pkg/iterator/zip_test.go
@@ -0,0 +1,59 @@
+package iterator
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vpraid/itertools/pkg/source"
+)
+
+func TestZip_Empty(t *testing.T) {
+	t.Parallel()
+
+	s := source.Slice([]int{})
+	it := Zip[int, int](s, s)
+	assert.False(t, it.Next())
+}
+
+func TestZip_Both(t *testing.T) {
+	t.Parallel()
+
+	s1 := source.Slice([]int{1, 2, 3})
+	s2 := source.Slice([]int{4, 5, 6})
+	it := Zip[int, int](s1, s2)
+	assert.True(t, it.Next())
+	assert.Equal(t, Pair[int, int]{1, 4}, it.Value())
+	assert.True(t, it.Next())
+	assert.Equal(t, Pair[int, int]{2, 5}, it.Value())
+	assert.True(t, it.Next())
+	assert.Equal(t, Pair[int, int]{3, 6}, it.Value())
+	assert.False(t, it.Next())
+}
+
+func TestZip_OneShorter(t *testing.T) {
+	t.Parallel()
+
+	s1 := source.Slice([]int{1, 2, 3})
+	s2 := source.Slice([]int{4, 5})
+	it := Zip[int, int](s1, s2)
+	assert.True(t, it.Next())
+	assert.Equal(t, Pair[int, int]{1, 4}, it.Value())
+	assert.True(t, it.Next())
+	assert.Equal(t, Pair[int, int]{2, 5}, it.Value())
+	assert.False(t, it.Next())
+}
+
+func ExampleZip() {
+	s1 := source.Slice([]int{1, 2, 3})
+	s2 := source.Slice([]int{4, 5, 6})
+	it := Zip[int, int](s1, s2)
+	for it.Next() {
+		pair := it.Value()
+		fmt.Println(pair.First, pair.Second)
+	}
+	// Output:
+	// 1 4
+	// 2 5
+	// 3 6
+}
