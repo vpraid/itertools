d233133b969f52f21afbf51ba5adc167d62ad063
Valerii Praid <valerii.praid@gmail.com>
2023-06-11 00:23:56 +0200

    Add literal source iterator

---
+++ pkg/source/literal.go
@@ -0,0 +1,29 @@
+package source
+
+// LiteralIterator is an iterator that iterates through the values of a slice given as a variadic argument list.
+type LiteralIterator[T any] struct {
+	*SliceIterator[T]
+}
+
+// Literal returns a LiteralIterator for the given argument list.
+func Literal[T any](args ...T) *LiteralIterator[T] {
+	return &LiteralIterator[T]{
+		SliceIterator: Slice(args),
+	}
+}
+
+// Next advances the iterator to the next value of the underlying slice. It returns false if the slice was exhausted.
+func (li *LiteralIterator[T]) Next() bool {
+	return li.SliceIterator.Next()
+}
+
+// Value returns the current value of the slice pointed by the iterator. If the slice was exhausted, it returns
+// the zero value.
+func (li *LiteralIterator[T]) Value() T {
+	return li.SliceIterator.Value()
+}
+
+// Collect returns the values of the slice collected into a slice.
+func (li *LiteralIterator[T]) Collect() []T {
+	return li.SliceIterator.Collect()
+}
---
+++ pkg/source/literal_test.go
@@ -0,0 +1,36 @@
+package source
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLiteralIterator_Empty(t *testing.T) {
+	t.Parallel()
+
+	it := Literal[int]()
+	assert.False(t, it.Next())
+	assert.Equal(t, 0, it.Value())
+}
+
+func TestLiteralIterator_NonEmpty(t *testing.T) {
+	t.Parallel()
+
+	it := Literal(1, 2, 3)
+	assert.True(t, it.Next())
+	assert.Equal(t, 1, it.Value())
+	assert.True(t, it.Next())
+	assert.Equal(t, 2, it.Value())
+	assert.True(t, it.Next())
+	assert.Equal(t, 3, it.Value())
+	assert.False(t, it.Next())
+	assert.Equal(t, 0, it.Value())
+}
+
+func TestLiteral_Collect(t *testing.T) {
+	t.Parallel()
+
+	it := Literal(1, 2, 3)
+	assert.Equal(t, []int{1, 2, 3}, it.Collect())
+}
