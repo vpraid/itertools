29634bdc58ca46a462af99cd19756bbaffd82443
Valerii Praid <valerii.praid@gmail.com>
2023-06-11 00:28:16 +0200

    Implement all and any

---
+++ pkg/functional/logic.go
@@ -0,0 +1,21 @@
+package functional
+
+import "github.com/vpraid/itertools/pkg/iterator"
+
+func Any[T any](source iterator.Iterator[T], predicate func(T) bool) bool {
+	for source.Next() {
+		if predicate(source.Value()) {
+			return true
+		}
+	}
+	return false
+}
+
+func All[T any](source iterator.Iterator[T], predicate func(T) bool) bool {
+	for source.Next() {
+		if !predicate(source.Value()) {
+			return false
+		}
+	}
+	return true
+}
---
+++ pkg/functional/logic_test.go
@@ -0,0 +1,22 @@
+package functional
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vpraid/itertools/pkg/source"
+)
+
+func TestAny(t *testing.T) {
+	t.Parallel()
+
+	assert.True(t, Any[int](source.Literal(1, 2, 3), func(i int) bool { return i == 2 }))
+	assert.False(t, Any[int](source.Literal(1, 2, 3), func(i int) bool { return i == 4 }))
+}
+
+func TestAll(t *testing.T) {
+	t.Parallel()
+
+	assert.True(t, All[int](source.Literal(1, 2, 3), func(i int) bool { return i > 0 }))
+	assert.False(t, All[int](source.Literal(1, 2, 3), func(i int) bool { return i < 3 }))
+}
