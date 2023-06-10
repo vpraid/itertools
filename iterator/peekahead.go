4d20b1ec29b564f3f067eb837bf769316612fc15
Valerii Praid <valerii.praid@gmail.com>
2023-06-10 11:59:54 +0200

    Implememt peek-ahead iterator

---
+++ iterator/peekahead.go
@@ -0,0 +1,40 @@
+package iterator
+
+// PeekAheadIterator is an iterator that allows reading the next element without advancing the iterator.
+type PeekAheadIterator[T any] struct {
+	Iterator[T]
+	value     T
+	peekValue T
+	exhausted bool
+}
+
+// PeekAhead returns a PeakAheadIterator.
+func PeekAhead[T any](it Iterator[T]) *PeekAheadIterator[T] {
+	exhausted := !it.Next()
+	return &PeekAheadIterator[T]{
+		Iterator:  it,
+		peekValue: it.Value(),
+		exhausted: exhausted,
+	}
+}
+
+// Next advances the iterator to the next element. It returns false if the underlying iterator was exhausted.
+// Under the hood it calls the underlying iterator's Next method to advance it one step further than what is
+// visible to the user, while caching the value of the current and next element.
+func (pai *PeekAheadIterator[T]) Next() bool {
+	pai.value = pai.Iterator.Value()
+	exhausted := pai.exhausted
+	pai.exhausted = !pai.Iterator.Next()
+	pai.peekValue = pai.Iterator.Value()
+	return !exhausted
+}
+
+// Value returns the cached value of the current element.
+func (pai *PeekAheadIterator[T]) Value() T {
+	return pai.value
+}
+
+// Peek returns the cached value of the next element.
+func (pai *PeekAheadIterator[T]) Peek() T {
+	return pai.peekValue
+}
---
+++ iterator/peekahead_test.go
@@ -0,0 +1,49 @@
+package iterator
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPeekAhead_Empty(t *testing.T) {
+	t.Parallel()
+
+	it := PeekAhead[int](Slice([]int{}))
+	assert.False(t, it.Next())
+}
+
+func TestPeekAhead_Some(t *testing.T) {
+	t.Parallel()
+
+	it := PeekAhead[int](Slice([]int{1, 2, 3}))
+	assert.True(t, it.Next())
+	assert.Equal(t, 1, it.Value())
+	assert.Equal(t, 2, it.Peek())
+	assert.True(t, it.Next())
+	assert.Equal(t, 2, it.Value())
+	assert.Equal(t, 3, it.Peek())
+	assert.True(t, it.Next())
+	assert.Equal(t, 3, it.Value())
+	assert.Equal(t, 0, it.Peek())
+	assert.False(t, it.Next())
+}
+
+func ExamplePeekAhead() {
+	s := Slice([]int{1, 2, 3})
+	// We need to specify the type of the iterator explicitly because the compiler cannot infer it yet. This is a known
+	// limitation of Go.
+	it := PeekAhead[int](s)
+	for it.Next() {
+		fmt.Println(it.Value())
+		fmt.Println(it.Peek())
+	}
+	// Output:
+	// 1
+	// 2
+	// 2
+	// 3
+	// 3
+	// 0
+}
