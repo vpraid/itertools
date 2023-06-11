f3a25a1781914ff5c2ace968ce8b26eca5ba0311
Valerii Praid <valerii.praid@gmail.com>
2023-06-11 11:21:47 +0200

    Implement count generator

---
+++ pkg/generator/count.go
@@ -0,0 +1,38 @@
+package generator
+
+import "golang.org/x/exp/constraints"
+
+type CountIterator[T constraints.Integer] struct {
+	start T
+	step  T
+	value T
+}
+
+// Count returns an iterator that counts up from the given start value.
+func Count[T constraints.Integer](start T) *CountIterator[T] {
+	return &CountIterator[T]{
+		start: start,
+		step:  1,
+		value: start,
+	}
+}
+
+// CountBy returns an iterator that counts up from the given start value with the given step.
+func CountBy[T constraints.Integer](start, step T) *CountIterator[T] {
+	return &CountIterator[T]{
+		start: start,
+		step:  step,
+		value: start,
+	}
+}
+
+// Next advances the iterator to the next element.
+func (ci *CountIterator[T]) Next() bool {
+	ci.value += ci.step
+	return true
+}
+
+// Value returns the current element.
+func (ci *CountIterator[T]) Value() T {
+	return ci.value - ci.step
+}
---
+++ pkg/generator/count_test.go
@@ -0,0 +1,39 @@
+package generator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCount(t *testing.T) {
+	t.Parallel()
+
+	ci := Count[int](0)
+	assert.True(t, ci.Next())
+	assert.Equal(t, 0, ci.Value())
+	assert.True(t, ci.Next())
+	assert.Equal(t, 1, ci.Value())
+	assert.True(t, ci.Next())
+	assert.Equal(t, 2, ci.Value())
+	assert.True(t, ci.Next())
+	assert.Equal(t, 3, ci.Value())
+	assert.True(t, ci.Next())
+	assert.Equal(t, 4, ci.Value())
+}
+
+func TestCountBy(t *testing.T) {
+	t.Parallel()
+
+	ci := CountBy[int](0, 2)
+	assert.True(t, ci.Next())
+	assert.Equal(t, 0, ci.Value())
+	assert.True(t, ci.Next())
+	assert.Equal(t, 2, ci.Value())
+	assert.True(t, ci.Next())
+	assert.Equal(t, 4, ci.Value())
+	assert.True(t, ci.Next())
+	assert.Equal(t, 6, ci.Value())
+	assert.True(t, ci.Next())
+	assert.Equal(t, 8, ci.Value())
+}
