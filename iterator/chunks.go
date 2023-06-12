17085c9f4d1cf2b52d8e518d3fad00367c598639
Valerii Praid <valerii.praid@gmail.com>
2023-06-12 02:22:28 +0200

    Make chunks detachable

--- iterator/chunks.go
+++ iterator/chunks.go
@@ -1,5 +1,7 @@
 package iterator
 
+import "github.com/vpraid/itertools/source"
+
 type ChunkIterator[T any] struct {
 	it    *PeekAheadIterator[T]
 	size  int
@@ -34,9 +36,15 @@ func (ci *ChunkIterator[T]) Next() bool {
 		}
 		return true
 	}
-	// Skip all elements in the current chunk until we find the next chunk.
-	for ci.chunk.Next() {
-		// Skip
+	// Skip all remaining elements in the current chunk until we find the next chunk.
+	// If the chunk is detached, we don't need to skip the elements, since the iterator is
+	// already pointing to the beginning of the next chunk.
+	if !ci.chunk.detached {
+		for size := ci.chunk.size; size != 0; size-- {
+			if !ci.it.Next() {
+				break
+			}
+		}
 	}
 	// We found the next chunk, so we can return true.
 	if !ci.it.Exhausted() {
@@ -58,8 +66,9 @@ func (ci *ChunkIterator[T]) Value() *Chunk[T] {
 
 // Chunk represents a group of consecutive elements of a ChunkIterator of a given size.
 type Chunk[T any] struct {
-	it   Iterator[T]
-	size int
+	it       Iterator[T]
+	size     int
+	detached bool
 }
 
 // Next advances the iterator and returns true if there is a next element, false otherwise.
@@ -79,4 +88,16 @@ func (c *Chunk[T]) Value() T {
 // Collect returns all elements of the chunk as a slice.
 func (c *Chunk[T]) Collect() []T {
 	return CollectFromIter[T](c)
+}
+
+// Detach separates the chunk from the underlying iterator. The elements of the chunk will be placed in a newly allocated
+// slice, and the chunk iterator will no longer read from the underlying iterator but instead will start reading
+// from the beginning of the newly allocated slice. Addtionally, detaching the chunk will advance the underlying iterator
+// to the end of the chunk.
+func (c *Chunk[T]) Detach() *Chunk[T] {
+	remainingSize := c.size
+	c.it = PeekAhead[T](source.Slice[T](c.Collect()))
+	c.size = remainingSize
+	c.detached = true
+	return c
 }
--- iterator/chunks_test.go
+++ iterator/chunks_test.go
@@ -49,6 +49,47 @@ func TestChunks_One(t *testing.T) {
 	assert.False(t, it.Next())
 }
 
+func TestChunks_Detach(t *testing.T) {
+	t.Parallel()
+
+	it := iterator.Chunks[int](source.Slice([]int{1, 2, 3, 4}), 2)
+	assert.True(t, it.Next())
+
+	c1 := it.Value().Detach()
+	assert.True(t, it.Next())
+	c2 := it.Value().Detach()
+	assert.False(t, it.Next())
+
+	assert.Equal(t, []int{1, 2}, c1.Collect())
+	assert.Equal(t, []int{3, 4}, c2.Collect())
+}
+
+func TestChunks_DetachWithNext(t *testing.T) {
+	t.Parallel()
+
+	it := iterator.Chunks[int](source.Slice([]int{1, 2, 3, 4, 5}), 2)
+	assert.True(t, it.Next())
+
+	c1 := it.Value()
+	c1.Next()
+	c1.Detach()
+	assert.True(t, it.Next())
+
+	c2 := it.Value()
+	c2.Next()
+	c2.Detach()
+	assert.True(t, it.Next())
+
+	c3 := it.Value()
+	c3.Next()
+	c3.Detach()
+	assert.False(t, it.Next())
+
+	assert.Equal(t, []int{2}, c1.Collect())
+	assert.Equal(t, []int{4}, c2.Collect())
+	assert.Equal(t, []int{}, c3.Collect())
+}
+
 func ExampleChunks() {
 	// This example demonstrates how to use Chunks to iterate over a slice in
 	// chunks of a given size.
